8e99b36ae94f1aeccf9608b623cebe8dc0c06fa7
johntran <tranjohn1211@gmail.com>
2023-05-02 21:53:30 -0700

    prep for solving the question

---
+++ arrays/sum.js
@@ -0,0 +1,12 @@
+// Create a function that returns the sum of the two lowest positive numbers given an array of minimum 4 positive integers. No floats or non-positive integers will be passed.
+
+// For example, when an array is passed like [19, 5, 42, 2, 77], the output should be 7.
+
+function sumTwoSmallestNumbers(numbers) {  
+    //Code here
+    // i word sort it to be smallest to highest
+    // i word store a number the first
+    // loop through the array and compare
+    // if the number is greater than the first number replace it
+    // return result 
+  }
