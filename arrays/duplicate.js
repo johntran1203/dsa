c027d4e1066222d7d941376a0af7457369c4f134
johntran <tranjohn1211@gmail.com>
2023-05-02 21:17:10 -0700

    solve with set

--- arrays/duplicate.js
+++ arrays/duplicate.js
@@ -1,5 +1,23 @@
 // Given an integer array nums, return true if any value appears at least twice in the array, and return false if every element is distinct.
 
+
+function containsDuplicate(nums) {
+    //Create an empty set to store the unique elements of the array(something to store the array)
+    const set = new Sset()
+    
+    // Iterate through each element in the array
+    for( const num of nums) {
+        //if the set already contains element return ture
+        if(set.has(num)) {
+            return true
+        }
+        //Otherwise add the current element to the set
+        set.add(num)
+    }
+    // if the loop completes without finding a duplicate return false
+    return false
+}
+
 //these are test case 
 // Input: nums = [1,2,3,1]
 // Output: true
