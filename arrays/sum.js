f9059d55993732eb65c33f0da53cf71da6dd97a0
johntran <tranjohn1211@gmail.com>
2023-05-02 22:00:28 -0700

    solve problem

--- arrays/sum.js
+++ arrays/sum.js
@@ -5,8 +5,18 @@
 function sumTwoSmallestNumbers(numbers) {  
     //Code here
     // i word sort it to be smallest to highest
+    const nums = numbers.sort((a,b) => a-b)
     // i word store a number the first
+    let result = nums[0] + nums[1]
     // loop through the array and compare
+    for(let i=2; i< numbers.length; i++) {
+        if(numbers[i] + numbers[i+1] < result) {
+            result = numbers[i] + numbers[i+1]
+        }
+    }
     // if the number is greater than the first number replace it
     // return result 
+    return result
   }
+
+console.log(sumTwoSmallestNumbers([15, 28, 4, 2, 43]))
